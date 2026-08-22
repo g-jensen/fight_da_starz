3f3618507237b045ececeabf141270f04c07e6f3
Emma Tchiorniy <emmtcho@gmail.com>
2026-08-22 17:26:59 +0200

    more renmaing

--- main.c
+++ main.c
@@ -7,28 +7,28 @@ struct gameState {
     int counter;
 };
 
-void set_window_buffer(struct gameState *state, struct abuf *ab, struct windowConfig *cfg) {
+void set_window_buffer(struct gameState *state, struct abuf *abuf, struct windowConfig *cfg) {
     int y;
     char str[100];
     snprintf(str,100,"I LOVE YOU x%d",state->counter);
     for (y = 0; y < cfg->screenrows; y++) {
-        abuf_append(ab, str, strlen(str));
+        abuf_append(abuf, str, strlen(str));
 
-        window_clear_line(ab);
+        window_clear_line(abuf);
         if (y < cfg->screenrows - 1) {
-            abuf_append(ab, "\r\n", 2);
+            abuf_append(abuf, "\r\n", 2);
         }
     }
 }
 
-void process_key(struct gameState *state, struct abuf *ab) {
+void process_key(struct gameState *state, struct abuf *abuf) {
     char c = window_read_key();
     switch (c) {
         case 'g':
             state->counter++;
             break;
         case CTRL_KEY('c'):
-            abuf_free(ab);
+            abuf_free(abuf);
             window_clear_screen();
             exit(0);
             break;
@@ -41,14 +41,14 @@ int main() {
     struct windowConfig cfg;
     window_init_config(&cfg);
 
-    struct abuf ab = ABUF_INIT;
+    struct abuf abuf = ABUF_INIT;
 
     struct gameState state = {.counter = 1};
 
     while (1) {
-        set_window_buffer(&state,&ab,&cfg);
-        window_draw_screen(&ab, &cfg);
-        process_key(&state, &ab);
+        set_window_buffer(&state,&abuf,&cfg);
+        window_draw_screen(&abuf, &cfg);
+        process_key(&state, &abuf);
     }
 
     return 0;
