9f72617e6676d7f40f6d2c3ca32a457b1a776370
Emma Tchiorniy <emmtcho@gmail.com>
2026-08-23 13:36:47 +0200

    add insert_string

--- core.c
+++ core.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "core.h"
 
 void die(const char *s) {
     perror(s);
     exit(1);
+}
+
+int insert_string(char* buf, char* string) {
+    int len = strlen(string);
+    for (int k = 0; k < len; k++) {
+        buf[k] = string[k];
+    }
+    return len;
 }
--- core.h
+++ core.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void die(const char *s);
+int insert_string(char* buf, char* string);
