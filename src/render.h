003a9754c4586a7ddad20a08baa64c922927f37d
Emma Tchiorniy <emmtcho@gmail.com>
2026-08-27 14:10:03 +0200

    add TODO by render_state_into_grid+

--- src/render.h
+++ src/render.h
@@ -7,4 +7,4 @@
 #define DEFAULT_CHAR ' '
 
 struct grid render_grid_create(int rows, int cols);
-void render_state_into_grid(struct gameState *state, struct grid grid);
+void render_state_into_grid(struct gameState *state, struct grid grid); // TODO - change to render_sprites_into_grid. `render` shouldn't know about `game`. maybe include `camera` abstraction.
