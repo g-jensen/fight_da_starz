ce8556275955184e7392f4d3ec713f78ec60102a
g-jensen <greg.micah.jensen@gmail.com>
2026-09-06 13:43:36 -0400

    move more load fns to gameobject_load

--- src/gameobject.h
+++ src/gameobject.h
@@ -4,17 +4,6 @@
 #include "sprite.h"
 #include "collision.h"
 
-struct gameObjectParseResult {
-    struct ipoint offset;
-    char* sprite_design;
-    char* collision_area_design;
-};
-
-struct gameObjectParseResult parse_game_object_file(char *filename);
-void game_object_parse_result_free(struct gameObjectParseResult *parse_result);
-struct sprite sprite_from_parsed_game_object(struct gameObjectParseResult *parse_result);
-collisionOffset collision_offset_from_parsed_game_object(struct gameObjectParseResult *parse_result);
-
 struct gameObject {
     struct fpoint position;
     struct fpoint velocity;
--- src/gameobject_load.h
+++ src/gameobject_load.h
@@ -2,6 +2,16 @@
 
 #include "gameobject.h"
 #include "resources.h"
+#include "sprite.h"
+#include "collision.h"
+
+struct gameObjectParseResult {
+    struct ipoint offset;
+    char* sprite_design;
+    char* collision_area_design;
+};
+
+struct gameObjectParseResult parse_game_object_file(char *filepath);
 
 struct gameObjectResources {
     struct sprite *sprite;
