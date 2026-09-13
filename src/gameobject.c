f05e7e57c2a93f644f02cedc9dff03c7b6d9f005
Emma Tchiorniy <emmtcho@gmail.com>
2026-09-13 18:29:29 +0200

    fix naming

--- src/gameobject.c
+++ src/gameobject.c
@@ -9,19 +9,19 @@ int iter_is_in_range(void *_state) {
     return state->idx < state->objects->length;
 }
 
-struct collisionArea object_collision_box(struct gameObject *object, struct ipoint object_offset) {
+struct collisionArea object_collision_area(struct gameObject *object, struct ipoint object_offset) {
     return (struct collisionArea){.offsets = object->collision_offset, .position = ipoint_add(to_ipoint(object->position),object_offset)};
 }
 
-struct collisionArea iter_next_collision_box(void *_state) {
+struct collisionArea iter_next_collision_area(void *_state) {
     struct gameObjectIterState *state = (struct gameObjectIterState*)_state;
     struct gameObject object = state->objects->items[state->idx++];
-    return object_collision_box(&object,(struct ipoint){});
+    return object_collision_area(&object,(struct ipoint){});
 }
 
 int does_object_overlap(struct gameObject *object, struct ipoint object_offset, struct gameObjects *collidables) {
     struct gameObjectIterState s = {.idx = 0, .objects = collidables};
-    struct collisionAreaIter iter = {.state = &s, .next = &iter_next_collision_box, .more = &iter_is_in_range};
-    struct collisionArea player_ca = object_collision_box(object, object_offset);
+    struct collisionAreaIter iter = {.state = &s, .next = &iter_next_collision_area, .more = &iter_is_in_range};
+    struct collisionArea player_ca = object_collision_area(object, object_offset);
     return collision_check_areas(&player_ca, &iter);
 }
